048d2fdd0bb0204ca09b0e8c7c69d1ef0c2c6f91
dellauliara <diella.aulia@gmail.com>
2022-06-11 19:29:13 +0700

    Done

--- promise.js
+++ promise.js
@@ -1,50 +1,44 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
+
 const promiseOutput = (emosi) => {
   return new Promise((resolve, reject) => {
     if (emosi == "") {
-      reject("Image not found!3");
+      reject("Invalid Data!");
     } else if (emosi == "marah") {
       (async () => {
-        const a = await promiseTheaterIXX();
-        const b = await promiseTheaterVGC();
-        let counterIXX = 0;
-        let counterVGC = 0;
-        for (let i = 0; i < a.length; i++) {
-          if (a[i].hasil == "marah") {
-            counterIXX++;
-          }
-        }
-        for (let i = 0; i < b.length; i++) {
-          if (b[i].hasil == "marah") {
-            counterVGC++;
-          }
-        }
-        resolve(counterIXX + counterVGC)
+        const sumIIX = hitung(await promiseTheaterIXX(), emosi);
+        const sumVGC = hitung(await promiseTheaterVGC(), emosi);
+        resolve(sumIIX + sumVGC);
       })();
-     } else if (emosi == "tidak marah") {
+    } else if (emosi == "tidak marah") {
       (async () => {
-        const a = await promiseTheaterIXX();
-        const b = await promiseTheaterVGC();
-        let counterIXX = 0;
-        let counterVGC = 0;
-        for (let i = 0; i < a.length; i++) {
-          if (a[i].hasil == "tidak marah") {
-            counterIXX++;
-          }
-        }
-        for (let i = 0; i < b.length; i++) {
-          if (b[i].hasil == "tidak marah") {
-            counterVGC++;
-          }
-        }
-        resolve(counterIXX + counterVGC)
+        const sumIIX = hitung(await promiseTheaterIXX(), emosi);
+        const sumVGC = hitung(await promiseTheaterVGC(), emosi);
+        resolve(sumIIX + sumVGC);
       })();
     }
   });
 };
 
+function hitung(data, feel) {
+  let counter = 0;
+  if (feel == "marah") {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].hasil == "marah") {
+        counter++;
+      }
+    }
+  } else {
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].hasil == "tidak marah") {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
 module.exports = {
   promiseOutput,
 };
